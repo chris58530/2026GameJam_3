0abca4abe6f6daf8d867b123750f4d1491bdc491
小仔 <71455438+chris58530@users.noreply.github.com>
2025-09-13 23:45:24 +0800

    修改聚光燈邏輯

--- Assets/Scripts/View/SpotLightView.cs
+++ Assets/Scripts/View/SpotLightView.cs
@@ -2,18 +2,21 @@ using System;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
+using DG.Tweening;
 
 public class SpotLightView : IView
 {
     [SerializeField] private GameObject root;
     [SerializeField] private AnimationCurve roundSpeedCurve;
-
+    [SerializeField] private float roundIdleTime;
     [Header("SpotLightObject")]
     [SerializeField] private SpotLight spotLights;
 
 
-    [Header("SpotLightImage")]
+    [Header("SpotLightUIImage")]
+    [SerializeField] private Image discoBallImage;
     [SerializeField] private Image spotLightImage;
+    private Vector3 originalScale;
 
     [Header("ColorSetting")]
     public ColorSetting[] colorSettings;
@@ -25,6 +28,11 @@ public class SpotLightView : IView
     {
         round = 0;
         onColorChange = action;
+        isPlaying = true;
+
+        if (spotLightImage != null)
+            originalScale = spotLightImage.transform.localScale;
+
         StartCoroutine(PlayingCoroutine());
         Debug.Log("StartColorProgress");
     }
@@ -34,12 +42,27 @@ public class SpotLightView : IView
         while (isPlaying)
         {
             float waitSec = roundSpeedCurve.Evaluate(round);
-            yield return new WaitForSeconds(waitSec);
-            round++;
+
             ColorSetting currentColor = colorSettings[UnityEngine.Random.Range(0, colorSettings.Length)];
-            spotLights.SetColor(currentColor.color);
+            spotLights.SetColor(Color.white);
             spotLightImage.color = currentColor.color;
             onColorChange?.Invoke(currentColor.gameColor);
+            discoBallImage.color = Color.white;
+
+            // 重置到原本大小，保持y和z不變
+            Vector3 resetScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+            spotLightImage.transform.localScale = resetScale;
+
+            // 使用DOTween讓x軸縮放到0.05f，保持y和z軸不變
+            float targetX = originalScale.x * 0.05f;
+            spotLightImage.transform.DOScaleX(targetX, waitSec);
+
+            yield return new WaitForSeconds(waitSec);
+            discoBallImage.color = currentColor.color;
+            spotLights.SetColor(currentColor.color);
+
+            round++;
+            yield return new WaitForSeconds(roundIdleTime);
         }
     }
 
